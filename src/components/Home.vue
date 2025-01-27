1e4132b96da36f65e5cdab6db8a64cc8b3cadfe0
TimothyKandiado <kandiadot@gmail.com>
2025-01-27 18:54:17 +0200

    Improved About, Added more elements to portfolio, improved timeline, added elevate to About and Timeline

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,8 @@
 <script setup>
 import lost_world_img from '@/assets/portfolio/the-lost-world.png'
+import avatar_img from '@/assets/avatar.jpg'
+import malalearn_splash from '@/assets/portfolio/malalearn.png'
+import medrec_img from '@/assets/portfolio/medrec.png'
 
 const languages = ["Rust", "Go", "C#", "Dart", "Python", "Javascript", "Typescript"]
 const frameworks = ["Vue.js", "Flutter", "Unity", "Godot", "Raylib", "Avalonia UI", "Blender"]
@@ -7,9 +10,34 @@ const projects = [
     {
         image: lost_world_img,
         title: "The Lost World",
-        description: "A 3D platformer made using Unity and C#",
+        description: "A 3D platformer made using Unity and C# made for a game jam on itch.io",
         link: "https://t-more1010.itch.io/lost-world"
-    }
+    },
+    {
+        image: malalearn_splash,
+        title: "Malalearn",
+        description: "An offline video learning app made using C# and Avalonia UI",
+        link: ""
+    },
+    {
+        image: medrec_img,
+        title: "MedRec",
+        description: "An electronic medical record storage app made using Flutter, Go and SQLite",
+        link: ""
+    },
+    {
+        image: "",
+        title: "Duplicheck",
+        description: "A cli tool for duplicate file checking and elimination implemented using Golang",
+        link: "https://github.com/TimothyKandiado/duplicheck"
+    },
+    {
+        image: "",
+        title: "NovaLang",
+        description: "A dynamically typed programming language that compiles down to register based vm bytecode implemented using Rust",
+        link: "https://github.com/TimothyKandiado/NovaLang"
+    },
+    
 ]
 
 const experience = [
@@ -42,6 +70,11 @@ const experience = [
     <!-- Hero -->
     <v-container id="hero" class="d-flex align-center justify-center h-50">
         <div>
+            <div class="d-flex align-center justify-space-around">
+                <v-avatar size="200">
+                    <v-img :src="avatar_img" alt="Timothy Kandiado"></v-img>
+                </v-avatar>
+            </div>
             <h1>Timothy Kandiado</h1>
             <p class="py-6"> Chemical Engineer | Game Developer | Fullstack Web Developer | Desktop & Mobile App Developer</p>
             <v-btn large href="#portfolio"> View My Work</v-btn>
@@ -49,12 +82,16 @@ const experience = [
     </v-container>
 
     <!-- About -->
-    <v-container id="about" class="py-12">
+    <v-container id="about" class="py-12 h-25 elevation-20">
         <v-row>
             <v-col cols="12" md="6">
                 <h2>About Me</h2>
-                <p>
-                    A BEng. in Chemical Engineering Graduate with First Class Honours.
+                <p class="text-body-1">
+                    I'm a Chemical Engineering graduate with a passion for software and game development. 
+                    I love combining creativity and problem-solving to build impactful applications 
+                    and immersive experiences.
+
+                    Let’s connect!
                 </p>
             </v-col>
         </v-row>
@@ -78,13 +115,25 @@ const experience = [
     <v-container id="portfolio" class="py-12">
         <h2>Portfolio</h2>
         <v-row>
-            <v-col cols="12" md="4" v-for="(project, index) in projects" :key="index">
+            <v-col cols="12" md="4" v-for="(project, index) in projects.slice(0, 3)" :key="index">
                 <v-card>
                     <v-img :src="project.image" height="200px" cover></v-img>
                     <v-card-title> {{ project.title }}</v-card-title>
                     <v-card-text> {{ project.description }}</v-card-text>
-                    <v-card-actions>
-                        <v-btn text :href="project.link" target="_blank">View</v-btn>
+                    <v-card-actions v-if="project.link">
+                        <v-btn  text :href="project.link" target="_blank">View</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" md="4" v-for="(project, index) in projects.slice(3, 6)" :key="index">
+                <v-card>
+                    <v-img :src="project.image" height="200px" cover></v-img>
+                    <v-card-title> {{ project.title }}</v-card-title>
+                    <v-card-text> {{ project.description }}</v-card-text>
+                    <v-card-actions v-if="project.link">
+                        <v-btn  text :href="project.link" target="_blank">View</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -92,12 +141,17 @@ const experience = [
     </v-container>
 
     <!-- Experience -->
-    <v-container id="experience" class="py-12">
+    <v-container id="experience" class="py-12 elevation-20">
         <h2>Experience</h2>
         <v-timeline side="end">
-            <v-timeline-item v-for="(item, index) in experience" :key="index" :dot-color="item.color">
+            <v-timeline-item 
+                v-for="(item, index) in experience" 
+                :key="index" 
+                :dot-color="item.color"
+                fill-dot
+                >
                 <v-card>
-                    <v-card-title> {{ item.title }}</v-card-title>
+                    <v-card-title :class="`bg-`+item.color+`-lighten-3`"> {{ item.title }}</v-card-title>
                     <v-card-text> {{ item.description }}</v-card-text>
                 </v-card>
             </v-timeline-item>
@@ -105,17 +159,12 @@ const experience = [
     </v-container>
 
     <!-- Contact -->
-    <v-container id="contact" class="py-12">
+    <v-container id="contact" class="py-16">
         <h2>Contact</h2>
         
         <a>
             <v-icon icon="mdi: mdi-email"></v-icon>
             kandiadotimothy@gmail.com
         </a>
-        <!--
-        <div class="social-links mt-4">
-            <v-icon v-for="link in links" :href="link.link" :color="link.color">{{ link.icon }}</v-icon>
-        </div>
-    -->
     </v-container>
 </template>
